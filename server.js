387eff36849c8cb4c6e93e6afd641813a4a2b057
Sebastian Udden <sebastian.udden@gmail.com>
2016-03-16 22:48:33 +0100

    Updated port for Heroku

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
+
+
 var middleware = require('./middleware.js');
-
 app.use(middleware.logger);
-
 app.get('/about', middleware.requireAuthentication, function (request, response) {
 	response.send('About Us!');
 });
