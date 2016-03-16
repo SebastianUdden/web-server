d61986808199a6ee07fcdf6eb5ff3b63fd6521f2
Sebastian Udden <sebastian.udden@gmail.com>
2016-03-16 21:44:47 +0100

    Initialize repo

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var middleware = {
+	requireAuthentication: function (request, response, next) {
+		console.log('Private route hit!');
+		next();
+	},
+	logger: function (request, response, next) {
+		// 
+		console.log('Request: ' + new Date().toString() + ' ' + request.method + ' ' + request.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (request, response) {
+	response.send('About us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function () {
+	console.log('Express server started on port: ' + port + '!');
+	console.log('Currently running...')
+});
+
